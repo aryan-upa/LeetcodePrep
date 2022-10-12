2926adcbb87416a13542b1c7bd4d049ac78198a7
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-13 01:48:57 +0530

    Time: 6 ms (88.56%), Space: 41.8 MB (47.39%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.java
+++ 0812-largest-triangle-area/0812-largest-triangle-area.java
@@ -1,5 +1,5 @@
 class Solution {
-static double maxArea = 0;
+    static double maxArea = 0;
 
 	public double largestTriangleArea(int[][] points) {
 		maxArea = 0;
@@ -9,8 +9,8 @@ static double maxArea = 0;
 
 	private void getAreaPerCombination(int[][] points, int currentPoint, int[][] triangle, int currI) {
 		if (currentPoint == 3) {
-			// System.out.println(Arrays.deepToString(triangle));
-			double area = getTriangleArea(triangle);
+            int[] p1 = triangle[0], p2 = triangle[1], p3 = triangle[2];
+            double area = (double) Math.abs(p1[0]*(p2[1] - p3[1]) + p2[0]*(p3[1] - p1[1]) + p3[0]*(p1[1] - p2[1])) / 2.0;
 			maxArea = Math.max(maxArea, area);
 			return;
 		}
@@ -20,10 +20,5 @@ static double maxArea = 0;
 			getAreaPerCombination(points, currentPoint + 1, triangle, i + 1);
 			triangle[currentPoint] = null;
 		}
-	}
-
-	private double getTriangleArea(int[][] triangle) {
-		int[] p1 = triangle[0], p2 = triangle[1], p3 = triangle[2];
-		return (double) Math.abs(p1[0]*(p2[1] - p3[1]) + p2[0]*(p3[1] - p1[1]) + p3[0]*(p1[1] - p2[1])) / 2.0;
 	}
 }

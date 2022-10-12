beb8e71b98f695eeeb0a471a2ebe2b005035b451
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-13 00:01:11 +0530

    Time: 14 ms (25.92%), Space: 54.1 MB (58.75%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -4,16 +4,11 @@ class Solution {
 
         int iniPos = nums.length - 3;
 		while (iniPos >= 0) {
-			if (nums[iniPos + 2] < nums[iniPos + 1] + nums[iniPos])
+			if (nums[iniPos + 2] < (nums[iniPos + 1] + nums[iniPos]))
 				return nums[iniPos] + nums[iniPos + 1] + nums[iniPos + 2];
 			iniPos--;
 		}
 
 		return 0;
 	}
-
-	public boolean isValidTriangle (int s1, int s2, int s3) {
-		return s1 < s2 + s3;
-	}
-
 }

c2fb3525a7e1ae5601fc81157134850e3b6c484b
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-15 23:28:35 +0530

    Time: 5 ms (15.37%), Space: 45.6 MB (5.95%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,12 +1,11 @@
 class Solution {
 	public List<List<Integer>> permute(int[] nums) {
 		List<List<Integer>> output = new ArrayList<>();
-		boolean[] isAdded = new boolean[nums.length];
-		permuteHelper(nums, isAdded, new ArrayList<>(), 0, output);
+		permuteHelper(nums, new ArrayList<>(), 0, output);
 		return output;
 	}
 
-	public void permuteHelper(int[] nums, boolean[] isAdded, List<String> res, int cCount, List<List<Integer>> output) {
+	public void permuteHelper(int[] nums, List<String> res, int cCount, List<List<Integer>> output) {
 		if (cCount == nums.length) {
 			ArrayList<Integer> temp = new ArrayList<>();
 
@@ -18,14 +17,15 @@ class Solution {
 		}
 
 		for (int i = 0; i < nums.length; i ++) {
-			if (!isAdded[i]) {
-				isAdded[i] = true;
+			if (nums[i] != Integer.MIN_VALUE) {
+				int temp = nums[i];
+				nums[i] = Integer.MIN_VALUE;
 
-				res.add("" + nums[i]);
-				permuteHelper(nums, isAdded, res, cCount + 1, output);
-				res.remove("" + nums[i]);
+				res.add("" + temp);
+				permuteHelper(nums, res, cCount + 1, output);
+				res.remove("" + temp);
 
-				isAdded[i] = false;
+				nums[i] = temp;
 			}
 		}
 	}

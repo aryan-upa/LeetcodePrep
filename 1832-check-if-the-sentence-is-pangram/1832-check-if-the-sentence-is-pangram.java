b3f70df79d094c2a6a8f6b09b58c49253dd9fd12
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-17 23:54:24 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (70.25%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean[] isPresent = new boolean[26];
-        int len = sentence.length();
-
-        for (int i = 0; i < len; i ++)
-            isPresent[sentence.charAt(i) - 97] = true;
-        
-        for (int i = 0; i < 26; i ++)
-            if (!isPresent[i])
+        for (int i = 0; i < 26; i ++) {
+            char c = (char) (97 + i);
+            if (sentence.indexOf(c) == -1)
                 return false;
-        
+        }
+
         return true;
     }
 }

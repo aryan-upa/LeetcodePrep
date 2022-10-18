37ee28e7c1b60d1816c960b0b590081e9b459d0b
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-18 07:17:06 +0530

    Time: 1176 ms (5.01%), Space: 118 MB (11.00%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,8 +2,8 @@ class Solution {
 	static boolean isFound = false;
 
 	public boolean exist(char[][] board, String word) {
-        if (board.length == 1 && board[0].length == 1)
-            return ("" + board[0][0]).equals(word);
+//         if (board.length == 1 && board[0].length == 1)
+//             return ("" + board[0][0]).equals(word);
         
         boolean[][] isVisited = new boolean[board.length][board[0].length];
         isFound = false;
@@ -17,19 +17,21 @@ class Solution {
 	}
 
 	public void existHelper(char[][] board, String word, String res, int let, int x, int y, boolean[][] isVisited) {
-		if (res.equals(word)) {
-			isFound = true;
-			return;
-		}
-
-        if (!isVisited[x][y]) {
+        if (!isFound && !isVisited[x][y] && board[x][y] == word.charAt(let)) {
             isVisited[x][y] = true;
-            if (board[x][y] == word.charAt(let)) {
-                if (x > 0) existHelper(board, word, res + word.charAt(let), let + 1, x - 1, y, isVisited);
-                if (x < board.length - 1) existHelper(board, word, res + word.charAt(let), let + 1, x + 1, y, isVisited);
-                if (y > 0) existHelper(board, word, res + word.charAt(let), let + 1, x, y - 1, isVisited);
-                if (y < board[0].length - 1) existHelper(board, word, res + word.charAt(let), let + 1, x, y + 1, isVisited);
+            res = res + word.charAt(let);
+            let += 1;
+
+            if (res.equals(word)) {
+                isFound = true;
+                return;
             }
+            
+            if (x > 0) 
+                existHelper(board, word, res, let, x - 1, y, isVisited);
+            if (x < board.length - 1) existHelper(board, word, res, let, x + 1, y, isVisited);
+            if (y > 0) existHelper(board, word, res, let, x, y - 1, isVisited);
+            if (y < board[0].length - 1) existHelper(board, word, res, let, x, y + 1, isVisited);
             isVisited[x][y] = false;
         }
 	}

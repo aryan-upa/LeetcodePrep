4bc4ff3a737222b6eeaaf73414e37cb9f77ed2e8
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-18 07:48:39 +0530

    Time: 146 ms (76.80%), Space: 41.2 MB (87.32%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,43 @@
 class Solution {
 	static boolean isFound = false;
-
+	static int wordLen;
+    
 	public boolean exist(char[][] board, String word) {
-       isFound = false;
+		isFound = false;
 
+		wordLen = word.length();
 		for (int i = 0; i < board.length && !isFound; i ++) {
 			for (int j = 0; j < board[0].length && !isFound; j ++)
-					existHelper(board, word, "", 0, i, j);
+				existHelper(board, word, 0, i, j);
 		}
 
 		return isFound;
 	}
 
-	public void existHelper(char[][] board, String word, String res, int let, int x, int y) {
-        if (!isFound && board[x][y] == word.charAt(let)) { 
-            board[x][y] ^= 256;
+	public void existHelper(char[][] board, String word, int let, int x, int y) {
+		if (board[x][y] == word.charAt(let)) {
+			board[x][y] ^= 256;
 
-            res = res + word.charAt(let);
-            let += 1;
+			let += 1;
 
-            if (res.equals(word)) {
-                isFound = true;
-                return;
-            }
-            
-            if (x > 0) 
-                existHelper(board, word, res, let, x - 1, y);
-            
-            if (x < board.length - 1)
-                existHelper(board, word, res, let, x + 1, y);
-            
-            if (y > 0)
-                existHelper(board, word, res, let, x, y - 1);
-            
-            if (y < board[0].length - 1)
-                existHelper(board, word, res, let, x, y + 1);
-            
-            board[x][y] ^= 256;
-        }
+			if (let == wordLen) {
+				isFound = true;
+				return;
+			}
+
+			if (x > 0 && !isFound)
+				existHelper(board, word, let, x - 1, y);
+
+			if (x < board.length - 1 && !isFound)
+				existHelper(board, word, let, x + 1, y);
+
+			if (y > 0 && !isFound)
+				existHelper(board, word, let, x, y - 1);
+
+			if (y < board[0].length - 1 && !isFound)
+				existHelper(board, word, let, x, y + 1);
+
+			board[x][y] ^= 256;
+		}
 	}
 }

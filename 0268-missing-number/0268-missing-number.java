cd8aa392f4b5458035fc4a1304e369191844cffe
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-22 03:36:27 +0530

    Time: 1 ms (86.57%), Space: 51.5 MB (24.99%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int xor = 0, max = nums.length;
-        
-        for (int i = 0; i <= max; i ++)
+
+        for (int i = 0; i < max; i ++) {
             xor ^= i;
-        
-        for (int i = 0; i <= max - 1; i ++)
             xor ^= nums[i];
+        }
         
-        return xor;
+        return xor ^= max;
+        
+//         for (int i = 0; i <= max; i ++)
+//             xor ^= i;
+        
+//         System.out.println(xor);
+        
+//         for (int i = 0; i <= max - 1; i ++)
+//             xor ^= nums[i];
+        
+//         return xor;
     }
 }

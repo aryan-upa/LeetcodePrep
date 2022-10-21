63da9df4352e47d9957ca230f7d1621397343188
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-22 02:27:10 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (15.25%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,24 +1,25 @@
 class Solution {
 	public int[] plusOne(int[] digits) {
-		int pos = digits.length - 1;
-
-		digits[pos]++;
-
-		int sum, carry = 0;
-		while (pos >= 0) {
-			sum = digits[pos] + carry;
-			digits[pos] = sum % 10;
-			carry = sum / 10;
-			pos --;
-		}
-
-		if (carry != 1)
-			return digits;
-
-		int[] res = new int[digits.length + 1];
-		res[0] = carry;
-		System.arraycopy(digits, 0, res, 1, digits.length);
-
-		return res;
+        int len = digits.length;
+        
+        if (digits[len - 1] != 9) {
+            digits[len - 1] ++;
+            return digits;            
+        }
+        
+        int pos = len - 1;
+        
+        while (pos >= 0 && digits[pos] == 9)
+            digits[pos --] = 0;
+        
+        if (pos == -1) {
+            int[] temp = new int[len + 1];
+            temp[0] = 1;
+            return temp;
+        }
+        
+        digits[pos] ++;
+        return digits;
+        
 	}
 }

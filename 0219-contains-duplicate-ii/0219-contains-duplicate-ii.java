c482e664a37a25357be33cbff92bcb28e5ea5aab
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-21 23:40:37 +0530

    Time: 144 ms (7.48%), Space: 97.6 MB (6.05%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -8,11 +8,15 @@ class Solution {
 		int h = 0;
 
 		for (int i = 0; i <= k && i < nums.length; i ++) {
-			if (table.get(nums[i]) == null)
+            
+            Integer val = table.get(nums[i]);
+            
+			if (val == null)
 				table.put(nums[i], 1);
 			else
-				table.put(nums[i], table.get(nums[i]) + 1);
-			if (table.get(nums[i]) == 2)
+				table.put(nums[i], val + 1);
+
+            if (table.get(nums[i]) == 2)
 				return true;
 		}
 
@@ -21,10 +25,12 @@ class Solution {
 		while (h < nums.length) {
 			table.put(nums[l], table.get(nums[l]) - 1);
 
-			if (table.get(nums[h]) == null)
+            Integer val = table.get(nums[h]);
+            
+			if (val == null)
 				table.put(nums[h], 1);
 			else
-				table.put(nums[h], table.get(nums[h]) + 1);
+				table.put(nums[h], val + 1);
 
 			if (table.get(nums[h]) == 2)
 				return true;

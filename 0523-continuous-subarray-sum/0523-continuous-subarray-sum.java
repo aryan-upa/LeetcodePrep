cf56f08e10a3b49af53425115765e64c10354013
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-10-26 11:18:50 +0530

    Time: 116 ms (5.15%), Space: 129.2 MB (5.04%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,24 +1,22 @@
 class Solution {
 	public boolean checkSubarraySum(int[] nums, int k) {
-		int[] mods = new int[nums.length + 1];
+        int sum = 0;
 
-		mods[0] = 0;
-		mods[1] = nums[0] % k;
-
-		Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
 		Map<Integer, Integer> map = new HashMap<>();
 
         set.add(0);
         map.put(0, 0);
 
         for (int i = 0; i < nums.length; i ++) {
-			mods[i+1] = (mods[i] + nums[i]) % k;
-			if (!set.contains(mods[i+1])) {
-				set.add(mods[i + 1]);
-				map.put(mods[i + 1], i + 1);
+			sum += nums[i];
+            int val = sum % k;
+			if (!set.contains(val)) {
+				set.add(val);
+				map.put(val, i + 1);
 			}
 			else {
-				if ((i + 1) - map.get(mods[i + 1]) >= 2)
+				if ((i + 1) - map.get(val) >= 2)
 					return true;
 			}
 		}

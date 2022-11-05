710190199ca20d0e002446749ee9bbfc6cbf7e04
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-11-05 18:55:05 +0530

    Time: 414 ms (5.00%), Space: 289.4 MB (5.00%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,36 +4,41 @@ class Solution {
 			return "";
 
 		Map<Character, Integer> map = new HashMap<>();
+		Set<Character> set = new HashSet<>();
 
-		for (int i = 0; i < t.length(); i ++)
+		for (int i = 0; i < t.length(); i ++) {
 			map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+			set.add(t.charAt(i));
+		}
 
 		int l, h, count;
 		l = h = 0;
+
 		boolean hasCountBeenZero = false;
 		String res = s;
-        int resLen = s.length();
+
+		int resLen = s.length();
 		count = map.size();
 
 		while (h < s.length()) {
 			char c = s.charAt(h);
-			if (map.containsKey(c)) {
-                int mapGet = map.get(c);
+			if (set.contains(c)) {
+				int mapGet = map.get(c);
 				map.put(c, mapGet - 1);
 				count = count - ((mapGet - 1) == 0 ? 1 : 0);
 			}
 
-			while (l < s.length() && (count == 0 || !map.containsKey(s.charAt(l)))) {
+			while (l < s.length() && (count == 0 || !set.contains(s.charAt(l)))) {
 
 				if (count == 0) {
 					hasCountBeenZero = true;
 					res = (h - l + 1) < resLen ? s.substring(l, h+1) : res;
-                    resLen = res.length();
+					resLen = res.length();
 				}
 
 				char c1 = s.charAt(l);
-				if (map.containsKey(s.charAt(l))) {
-                    int mapGet = map.get(c1);
+				if (set.contains(s.charAt(l))) {
+					int mapGet = map.get(c1);
 					map.put(c1, mapGet + 1);
 					if ((mapGet+1) > 0)
 						count ++;

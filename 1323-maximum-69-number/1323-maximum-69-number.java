1fd276fc32a4045a17b090de7119fb504ea9715c
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-11-08 10:49:14 +0530

    Time: 2 ms (48.37%), Space: 41.4 MB (27.41%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,40 +1,22 @@
 class Solution {
-    public int maximum69Number (int num) {        
-        switch ("" + num) {
-            case "9": return 9;
-            case "6": return 9;
-            
-            case "99": return 99;
-            case "96": return 99;
-            case "66": return 96;
-            case "69": return 99;
-            
-            case "999": return 999;
-            case "996": return 999;
-            case "969": return 999;
-            case "966": return 996;
-            case "699": return 999;
-            case "696": return 996;
-            case "669": return 969;
-            case "666": return 966;
-
-			case "9999": return 9999;
-			case "9996": return 9999;
-			case "9969": return 9999;
-			case "9699": return 9999;
-			case "6999": return 9999;
-			case "9966": return 9996;
-			case "9696": return 9996;
-			case "6996": return 9996;
-			case "9669": return 9969;
-			case "6969": return 9969;
-			case "9666": return 9966;
-			case "6966": return 9966;				
-			case "6699": return 9699;
-			case "6696": return 9696;
-			case "6669": return 9669;
-			case "6666": return 9666;
-        }
-        return 0;
+    public int maximum69Number (int num) {
+		return switch ("" + num) {
+			case "9", "6" -> 9;
+			case "99", "96", "69" -> 99;
+			case "66" -> 96;
+			case "999", "996", "969", "699" -> 999;
+			case "966", "696" -> 996;
+			case "669" -> 969;
+			case "666" -> 966;
+			case "9999", "9996", "9969", "9699", "6999" -> 9999;
+			case "9966", "9696", "6996" -> 9996;
+			case "9669", "6969" -> 9969;
+			case "9666", "6966" -> 9966;
+			case "6699" -> 9699;
+			case "6696" -> 9696;
+			case "6669" -> 9669;
+			case "6666" -> 9666;
+			default -> 0;
+		};
     }
 }

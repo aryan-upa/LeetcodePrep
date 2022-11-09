71befbf2b3b62eca30310c787329d0a05638b6df
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-11-09 19:45:54 +0530

    Time: 3 ms (90.06%), Space: 45.9 MB (71.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -5,28 +5,29 @@ class Solution {
         
         // row check
         for (int i = 0; i < rmax; i ++) {
-            int[] occ = new int[10];
-            for (int j = 0; j < cmax; j ++)
-                if (b[i][j] != '.') occ[b[i][j] - 48] ++;
-            
-            for (int count : occ)
-                if (count > 1)
+            int[] occ = new int[12];
+            for (int j = 0; j < cmax; j ++) {
+                if (b[i][j] != '.' && occ[b[i][j] - 46] == 1)
                     return false;
+                else
+                    occ[b[i][j] - 46]++;
+            }
         }
 
         // column check
         for (int i = 0; i < cmax; i ++) {
-            int[] occ = new int[10];
-            for (int j = 0; j < rmax; j ++)
-                if (b[j][i] != '.') occ[b[j][i] - 48] ++;
-            
-            for (int count : occ)
-                if (count > 1)
+            int[] occ = new int[12];
+            for (int j = 0; j < rmax; j ++) {
+                if (b[j][i] != '.' && occ[b[j][i] - 46] == 1)
                     return false;
+                else
+                    occ[b[j][i] - 46]++;
+            }
         }        
         
         int[] r = new int[]{0, 3, 6};
         
+        // box check
         for (int i = 0; i < 3; i ++) {
             for (int j = 0; j < 3; j ++) {
                 if (!checkBox(r[i], r[j], b))
@@ -38,15 +39,15 @@ class Solution {
     }
     
     public static boolean checkBox(int r, int c, char[][] b) {
-        int[] occ = new int[10]; 
+        int[] occ = new int[12]; 
         
         for (int i = r; i < r+3; i ++)
-            for (int j = c; j < c+3; j++)
-                if (b[i][j] != '.') occ[b[i][j] - 48]++;
-        
-        for (int count : occ)
-            if (count > 1)
-                return false;
+            for (int j = c; j < c+3; j++) {
+                if (b[i][j] != '.' && occ[b[i][j] - 46] == 1)
+                    return false;
+                else
+                    occ[b[i][j] - 46]++;
+            }
         
         return true;
     }

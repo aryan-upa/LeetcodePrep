9fc88743972a20389fe4b35722d4707cb28a9a9b
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-11-17 23:59:51 +0530

    Time: 2 ms (93.13%), Space: 56.6 MB (27.49%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+// Moore's Voting Algorithm
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        return findCandidate(nums);
+    }
+    
+    public int findCandidate(int[] nums) {
+        int maj_ind = 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i ++) {
+            count += (nums[i] == nums[maj_ind]) ? 1 : -1;
+            if (count == 0) {
+                count = 1;
+                maj_ind = i;
+            }
+        }
+        
+        return nums[maj_ind];
+    }
+}

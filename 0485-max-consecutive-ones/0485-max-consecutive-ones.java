699eff8d8efe2aea9e534f53e4ad896d8efe66c9
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2022-12-03 13:27:48 +0530

    Time: 4 ms (62.53%), Space: 56.7 MB (45.42%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,8 +3,8 @@ class Solution {
         int max = 0;
         int curr = 0;
         
-        for (int i = 0; i < nums.length; i ++) {
-            curr = nums[i] == 1 ? curr + 1 : 0;
+        for (int i : nums) {
+            curr = i == 1 ? curr + 1 : 0;
             max = max > curr ? max : curr;
         }
         

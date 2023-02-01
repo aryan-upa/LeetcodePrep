acba4d5d11556ef759b0d01330be2105aca92fb4
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-02-01 10:09:12 +0530

    Time: 1 ms (85.83%), Space: 42.3 MB (55.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,26 +1,17 @@
 class Solution {
+    public boolean isGCDAble (String s1, String s2) {
+        return !(s1 + s2).equals(s2 + s1);
+    }
+    
+    public int gcd (int a, int b) {
+        return b == 0 ? a : gcd (b, a % b);
+    }
+    
     public String gcdOfStrings(String str1, String str2) {
-        String gcd = "";
-        String ans = "";
+        if (isGCDAble(str1, str2))
+            return "";
         
-        int l1 = str1.length();
-        int l2 = str2.length();
-        
-        int minLen = l1 < l2 ? l1 : l2;
-        int i = 0;
-        
-        while (i < minLen && str1.charAt(i) == str2.charAt(i)) {
-            gcd += str1.charAt(i);
-            int gcdLen = i + 1;
-            
-            if (l1%gcdLen == 0 && l2%gcdLen == 0) {
-                if (gcd.repeat(l1/gcdLen).equals(str1) && gcd.repeat(l2/gcdLen).equals(str2))
-                    ans = gcd;
-            }
-            
-            i += 1;
-        }
-        
-        return ans;
+        int gcdLen = gcd (str1.length(), str2.length());
+        return str1.substring(0, gcdLen);
     }
 }

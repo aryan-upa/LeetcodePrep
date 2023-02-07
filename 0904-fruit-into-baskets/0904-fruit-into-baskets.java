ccebec6f33252bb93e1e1fb4d0381890334cf0b5
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-02-07 22:15:00 +0530

    Time: 70 ms (27.13%), Space: 49.7 MB (99.75%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,28 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-		int start = 0;
-		int end = 0;
+		int start, end;
+		start = end = 0;
 
-		int[] fCount = new int[fruits.length];
-		int types = 0;
-		int maxLen = 0;
+		HashMap<Integer, Integer> fruitTypeAndCount = new HashMap<>();
+		int maxBasket = 0;
+
 		while (end < fruits.length) {
-			if (fCount[fruits[end]] == 0)
-				types += 1;
+			if (!fruitTypeAndCount.containsKey(fruits[end]))
+				fruitTypeAndCount.put(fruits[end], 0);
 
-			fCount[fruits[end]] += 1;
+			fruitTypeAndCount.put(fruits[end], fruitTypeAndCount.get(fruits[end]) + 1);
 
-			while (types > 2 && start < fruits.length) {
-				fCount[fruits[start]] -= 1;
-				if (fCount[fruits[start]] == 0)
-					types -= 1;
-				start ++;
+			while (fruitTypeAndCount.keySet().size() > 2) {
+				fruitTypeAndCount.put(fruits[start], fruitTypeAndCount.get(fruits[start]) - 1);
+				if (fruitTypeAndCount.get(fruits[start]) == 0)
+					fruitTypeAndCount.remove(fruits[start]);
+				start += 1;
 			}
 
-			maxLen = Math.max(maxLen, end - start + 1);
+			maxBasket = Math.max(maxBasket, end - start + 1);
 			end ++;
 		}
 
-		return maxLen;
+		return maxBasket;
+
     }
 }

3a464e5b48dc4ee8fd5c2d73e44a1382c75dd3d1
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-03-15 09:29:55 +0530

    Time: 21 ms (25.17%), Space: 47.2 MB (18.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,20 +5,19 @@ class Solution {
         for (int i = 0; i < strings.length; i++) {
 			String string = strings[i];
 			String sorted = sortedString(string);
-			if (map.containsKey(sorted))
-				map.get(sorted).add(string);
-			else {
+			if (!map.containsKey(sorted))
 				map.put(sorted, new ArrayList<>());
-				map.get(sorted).add(string);
-			}
+            map.get(sorted).add(string);
 		}
 
 		return new ArrayList<>(map.values());
 	}
 
 	private String sortedString(String string) {
-		char[] arr = string.toCharArray();
-		Arrays.sort(arr);
-		return new String(arr);
+        int[] hash = new int[26];
+        for (int i = 0; i < string.length(); i ++)
+            hash[string.charAt(i) - 'a'] += 1;
+        
+		return Arrays.toString(hash);
 	}
 }

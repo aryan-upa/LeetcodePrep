ebad566eb1ac3b9a1dabd04bb601947511928f60
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-03-19 21:35:47 +0530

    Time: 3 ms (52.99%), Space: 42.6 MB (53.93%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -6,13 +6,15 @@ class Solution {
     private int[] intersection(int[] nums1, int[] nums2) {
 		Map<Integer, Integer> mapFreq = new HashMap<>();
 
-		for (int i : nums1) {
+		for (int idx = 0; idx < nums1.length; idx ++) {
+            int i = nums1[idx];
 			int v = mapFreq.computeIfAbsent(i, val -> 0);
             mapFreq.put(i, v + 1);
 		}
 
 		ArrayList<Integer> intersection = new ArrayList<>();
-		for (int i : nums2) {
+		for (int idx = 0; idx < nums2.length; idx ++) {
+            int i = nums2[idx];
             int fq = mapFreq.getOrDefault(i, 0);
 			if (fq > 0) {
 				intersection.add(i);

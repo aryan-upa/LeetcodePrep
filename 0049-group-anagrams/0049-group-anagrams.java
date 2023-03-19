c2c42ed04f12369d863df424da39227642a283e6
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-03-19 23:13:24 +0530

    Time: 6 ms (98.29%), Space: 45.2 MB (87.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,9 +1,20 @@
 class Solution {
+    
+    static private int[] primes;
+    
+    static {
+        primes = new int[] {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+            31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+            73, 79, 83, 89, 97, 101
+        };
+    }
+    
 	public List<List<String>> groupAnagrams(String[] strings) {
-		Map<String, List<String>> map = new HashMap<>();
+		Map<Long, List<String>> map = new HashMap<>();
 
 		for (String s : strings) {
-			String mapper = getStringMapper(s);
+			Long mapper = getStringMapper(s);
 			List<String> ls = map.computeIfAbsent(mapper, m -> new ArrayList<>());
 			ls.add(s);
 		}
@@ -11,13 +22,15 @@ class Solution {
 		return new ArrayList<>(map.values());
 	}
 	
-	private String getStringMapper (String s) {
-		int[] occ = new int[26];
+	private long getStringMapper (String s) {
+        long prod = 1;
 		int len = s.length();
 		
-		for (int i = 0; i < len; i ++)
-			occ[s.charAt(i) - 'a'] ++;
+		for (int i = 0; i < len; i ++) {
+			prod *= primes[s.charAt(i) - 'a'];
+            prod %= Long.MAX_VALUE;
+        }
 		
-		return Arrays.toString(occ);
+		return prod;
 	}
 }

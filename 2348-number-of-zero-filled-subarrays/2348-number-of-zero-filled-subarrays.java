fcbffa1b677afa448332bd917db03174bafed94a
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-03-21 09:03:46 +0530

    Time: 3 ms (99.69%), Space: 60.5 MB (16.93%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -3,14 +3,9 @@ class Solution {
         long total = 0;
         long zCount = 0;
         
-        for (int i = 0; i < nums.length; i ++) {
-            if (nums[i] == 0) {
-                zCount += 1;
-                total += zCount;
-                continue;
-            }
-            
-            zCount = 0;
+        for (int i : nums) {
+            zCount = i == 0 ? zCount + 1 : 0;
+            total += zCount;
         }
         
         return total;

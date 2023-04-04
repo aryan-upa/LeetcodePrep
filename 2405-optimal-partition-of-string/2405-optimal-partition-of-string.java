575fb70e19cbfee1418ff823c39f36d08ab0228a
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-04-04 09:31:43 +0530

    Time: 14 ms (77.74%), Space: 43 MB (75.16%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,25 +1,24 @@
 class Solution {
     public int partitionString(String s) {
-        char[] ch = s.toCharArray();
         int[] chars = new int[26];
         int i = 0;
         int e = 0;
         int stringCount = 0;
         
-        while (e < ch.length) {
-            chars[ch[e] - 97] += 1;
+        while (e < s.length()) {
+            chars[s.charAt(e) - 97] += 1;
             
-            if (chars[ch[e] - 97] > 1) {
+            if (chars[s.charAt(e) - 97] > 1) {
                 chars = new int[26];
                 stringCount += 1;
-                chars[ch[e] - 97] += 1;
+                chars[s.charAt(e) - 97] += 1;
                 i = e;
             }
             
             e += 1;
         }
         
-        if (i!=e)
+        if (i != e)
             stringCount += 1;
         
         return stringCount;

76081f63efe0c626b9d12a1e4ce5678ab48778cc
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-07-03 22:14:15 +0530

    Time: 4 ms (50.57%), Space: 41.7 MB (57.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean buddyStrings (String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
+        
+        if (s.equals(goal)) {
+            HashSet<Character> set = new HashSet<>();
+            
+            for (char c : s.toCharArray()) set.add(c);
+            
+            return set.size() < s.length();
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i ++)
+            if (s.charAt(i) != goal.charAt(i))
+                list.add(i);
+        
+        return 
+            list.size() == 2 &&
+            s.charAt(list.get(0)) == goal.charAt(list.get(1)) && 
+            s.charAt(list.get(1)) == goal.charAt(list.get(0))
+            ;
+        
+        
+    }
+}

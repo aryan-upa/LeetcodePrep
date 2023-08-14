c67c638b342f849144f138d4682b92dd2310b4bd
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-08-14 08:00:04 +0530

    Time: 32 ms (62.04%), Space: 60.5 MB (6.69%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,17 +1,33 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < k; i ++) heap.add(nums[i]);
+        for (int num : nums) list.add(num);
+        return getMid (list, k);
+    }
+    
+    public int getMid (List<Integer> list, int k) {
+        if (list.size() == 1)
+            return list.get(0);
         
-        for (int i = k; i < nums.length; i ++) {
-            if (nums[i] <= heap.peek())
-                continue;
-            
-            heap.poll();
-            heap.add(nums[i]);
+        List<Integer> 
+            left = new ArrayList<>(), 
+            right = new ArrayList<>(), 
+            mid = new ArrayList<>();
+        
+        int pivot = list.get(list.size() / 2);
+        
+        for (int el : list) {
+            if (el > pivot) left.add(el);
+            else if (el == pivot) mid.add(el);
+            else right.add(el);
         }
         
-        return heap.peek();
+        if (left.size() >= k)
+            return getMid (left, k);
+        else if (left.size() + mid.size() < k)
+            return getMid (right, k - left.size() - mid.size());
+        else
+            return pivot;
     }
 }

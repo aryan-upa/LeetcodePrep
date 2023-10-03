887d19648f741640ea8055f7f4a665c4890d9032
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-10-03 20:12:30 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (85.07%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,37 +1,37 @@
 class Solution {
-//     public int numIdenticalPairs(int[] nums) {
-//         int[] occ = new int[101];
-        
-//         for (int i : nums) 
-//             occ[i] += 1;
-        
-//         int count = 0;
-//         for (int i : occ)
-//             count += i == 0 || i == 1 ? 0 : i * (i - 1) / 2;
-        
-//         return count;
-//     }    
-        
     public int numIdenticalPairs(int[] nums) {
-        if (nums == null)
-            throw new NullPointerException("Array is Null!");
-        
         int[] occ = new int[101];
         
-        for (int i : nums)
+        for (int i : nums) 
             occ[i] += 1;
         
-        int goodPairCount = 0;
-        for (int i = 0; i < occ.length; i ++) {
-            int occOfI = occ[i];
-            
-            if (occOfI == 0 || occOfI == 1)
-                continue;
-            
-            int goodPairCountForI = occOfI * (occOfI - 1) / 2;
-            goodPairCount += goodPairCountForI;
-        }
+        int count = 0;
+        for (int i : occ)
+            count += i == 0 || i == 1 ? 0 : i * (i - 1) / 2;
         
-        return goodPairCount;
-    }
+        return count;
+    }    
+        
+//     public int numIdenticalPairs(int[] nums) {
+//         if (nums == null)
+//             throw new NullPointerException("Array is Null!");
+        
+//         int[] occ = new int[101];
+        
+//         for (int i : nums)
+//             occ[i] += 1;
+        
+//         int goodPairCount = 0;
+//         for (int i = 0; i < occ.length; i ++) {
+//             int occOfI = occ[i];
+            
+//             if (occOfI == 0 || occOfI == 1)
+//                 continue;
+            
+//             int goodPairCountForI = occOfI * (occOfI - 1) / 2;
+//             goodPairCount += goodPairCountForI;
+//         }
+        
+//         return goodPairCount;
+//     }
 }

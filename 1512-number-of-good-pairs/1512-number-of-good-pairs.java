2b6662a554f9036dee4ed0a5194b156846670587
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2023-10-03 20:12:12 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (93.04%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,5 +1,21 @@
 class Solution {
+//     public int numIdenticalPairs(int[] nums) {
+//         int[] occ = new int[101];
+        
+//         for (int i : nums) 
+//             occ[i] += 1;
+        
+//         int count = 0;
+//         for (int i : occ)
+//             count += i == 0 || i == 1 ? 0 : i * (i - 1) / 2;
+        
+//         return count;
+//     }    
+        
     public int numIdenticalPairs(int[] nums) {
+        if (nums == null)
+            throw new NullPointerException("Array is Null!");
+        
         int[] occ = new int[101];
         
         for (int i : nums)
